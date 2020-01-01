75050d604f22a1335d6b70a34b4c539aa7959509
Matt Obert on Windows <matt@hotwebmatter.com>
2020-01-01 11:38:28 -0500

    Use static System.Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace park_classes
 {
@@ -6,7 +7,7 @@ namespace park_classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
         }
     }
 }
