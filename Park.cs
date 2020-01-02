38c89bd982559952ad8fa2eb256174eb122933a7
Matt Obert on Windows <matt@hotwebmatter.com>
2020-01-02 09:39:45 -0500

    Declare private data members

--- Park.cs
+++ Park.cs
@@ -6,5 +6,13 @@ namespace park_classes
 {
     class Park
     {
+        // data members
+        private string name,
+                       location,
+                       facilityType;
+        private double feePerVisitor,
+                       annualBudget;
+        private int numberOfEmployees,
+                    visitorsLastYear;
     }
 }
